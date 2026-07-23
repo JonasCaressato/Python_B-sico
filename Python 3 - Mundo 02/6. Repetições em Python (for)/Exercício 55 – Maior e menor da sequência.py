8e0dff7cb2db1c0bf05925cac2d17dc5c75f96c9
Jonas <jonasca.jesus@gmail.com>
2026-07-22 22:30:34 -0300

    Adição das atividades

--- Python 3 - Mundo 02/6. Repetições em Python (for)/Exercício 55 – Maior e menor da sequência.py
+++ Python 3 - Mundo 02/6. Repetições em Python (for)/Exercício 55 – Maior e menor da sequência.py
@@ -3,3 +3,24 @@ Enunciado Exercício 55 – Maior e menor da sequência
 
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 """
+#Definição dos pesos base
+maior_peso = 0
+menor_peso = 0
+
+for pessoa in range(1,6):
+    peso = float(input('Qual o seu peso? '))
+
+    #Definindo o primeiro peso lido como menor e maior peso
+    if maior_peso <= 0 and menor_peso <= 0:
+        maior_peso = peso
+        menor_peso = peso
+
+    #Comparando valores e definindo qual o maior e o menor peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+        if peso < menor_peso:
+            menor_peso = peso
+
+print(f'O MAIOR peso digitado é: {maior_peso:.2f}')
+print(f'O MENOR peso digitado é: {menor_peso:.2f}')
--- Python 3 - Mundo 02/6. Repetições em Python (for)/Exercício 56 – Analisador completo.py
+++ Python 3 - Mundo 02/6. Repetições em Python (for)/Exercício 56 – Analisador completo.py
@@ -4,3 +4,44 @@ Enunciado Exercício 56 – Analisador completo
 Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
 a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 """
+
+#Atribuição de valores-base
+media_de_idade = 0
+
+homem_mais_velho = ''
+idade_do_homem_mais_velho = 0
+
+total_de_mulheres_com_menos_de_20_anos = 0
+
+for n in range (1, 5):
+
+    #Obtendo os dados para a analise
+    print('-' * 20)
+    nome = str(input(f'Digite o NOME do {n}° integrante: '))
+    idade = int(input(f'Digite a IDADE do {n}° integrante: '))
+    sexo = str(input(f'Digite o SEXO do {n}° integrante [M/F]: '))
+
+    #Obtendo a média de idade do grupo
+    media_de_idade = idade / n
+
+    #Homem mais velho do grupo
+    if sexo == 'M':
+        if homem_mais_velho == '':
+            homem_mais_velho = nome
+            idade_do_homem_mais_velho = idade
+        else:
+            if idade > idade_do_homem_mais_velho:
+                idade_do_homem_mais_velho = idade
+                homem_mais_velho = nome
+
+    #Total de mulheres com menos de 20 anos
+    elif sexo == 'F':
+        if idade < 20:
+            total_de_mulheres_com_menos_de_20_anos += 1
+
+
+print('-' * 20)
+print(f'A média de idade é: {media_de_idade}!')
+print(f'{homem_mais_velho} é o homem mais velho com {idade_do_homem_mais_velho} anos!')
+print(f'Há um total de {total_de_mulheres_com_menos_de_20_anos} mulheres com menos de 20 anos!')
+print('-' * 20)
